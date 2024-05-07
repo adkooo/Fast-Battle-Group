a30b603d2484f23759fc89b249a74e5b9e7b6a60
adko <154256737+adkooo@users.noreply.github.com>
2024-05-07 09:51:01 -0700

    Add files via upload

---
+++ start.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour
+{
+    private float timer = 0f;
+    public float changeInterval = 50f; // تعیین فاصله زمانی بین تغییر صفحه
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // اگر زمان مساوی یا بیشتر از فاصله زمانی مورد نظر بود
+        if (timer >= changeInterval)
+        {
+            // تغییر به سین دوم
+            SceneManager.LoadScene("menu");
+
+            // تنظیم مجدد تایمر
+            timer = 0f;
+        }
+    }
+}
